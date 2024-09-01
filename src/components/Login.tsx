eac8d1388d02c6c52ab89b485493e97c90649e6a
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-09-01 13:55:41 -0300

    adicionando o home da pagina

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -26,9 +26,40 @@ const Login: React.FC = () => {
   
     if (username) {
       return (
-        <div className="p-4 text-center">
-          <h1 className="text-2xl font-bold">Olá, {username}! Bem-vindo ao sistema.</h1>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+          <header className="w-full text-center p-6">
+            <h1 className="text-3xl font-bold">
+              Olá,  
+              <span className="text-green-500"> {username} </span> ! Bem-vindo ao sistema.</h1>
+          </header>
+      
+          <main className="flex-1 flex flex-col items-center justify-center text-center space-y-6">
+            <div>
+              <h1 className="text-4xl font-semibold text-white mt-2">Eu sou Gustavo Rodrigues</h1>
+
+              <div className="mt-4 text-2xl font-medium">
+                <span className="inline-block">Desenvolvedor Backend especializado em</span>
+                <div className="text-green-400 animate-pulse">
+                  Java, Spring Boot, MySQL, PostgreSQL
+                </div>
+              </div>
+            </div>
+
+            <p className="max-w-2xl text-lg text-gray-400 leading-relaxed">
+              Criando soluções escaláveis e robustas com foco em performance e segurança.
+            </p>
+
+            <a
+              href="https://wa.me/55219080022571"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-4 inline-block bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-8 rounded transition-transform duration-300 ease-in-out transform hover:scale-105"
+            >
+              Contate-me
+            </a>
+          </main>
         </div>
+      
       );
     }
   
